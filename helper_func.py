42ee3ff752acd1d61304889b1349bbf4abf07d48
Proloottricks <ritiksakhala17@gmail.com>
2024-08-03 17:26:37 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -113,7 +113,7 @@ async def get_shortlink(url, api, link):
     return link
 
 def get_exp_time(seconds):
-    periods = [('days', 86400), ('hours', 3600), ('mins', 60), ('secs', 1)]
+    periods = [('days', 64800), ('hours', 3600), ('mins', 60), ('secs', 1)]
     result = ''
     for period_name, period_seconds in periods:
         if seconds >= period_seconds:
